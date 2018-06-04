234f5193069d028dbbe4ee0f14a42cae709bcb92
hatelove <jay.hatelove@gmail.com>
2018-06-04 23:22:31 +0800

    init

---
+++ UnitTestProject20/Budget.cs
@@ -0,0 +1,7 @@
+﻿using System;
+
+internal class Budget
+{
+    public DateTime FirstDay { get; set; }
+    public DateTime LastDay { get; set; }
+}
---
+++ UnitTestProject20/DateRange.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+internal class BudgetModel
+{
+    public decimal TotalAmount()
+    {
+        var budget = new Budget { FirstDay = new DateTime(2018, 4, 1), LastDay = new DateTime(2018, 4, 30) };
+
+        var dateRange = new DateRange(new DateTime(2018, 3, 31), new DateTime(2018, 5, 1));
+
+        var overlappingDays = GetDays(dateRange, budget.FirstDay, budget.LastDay);
+
+        return 0;
+    }
+
+    private int GetDays(DateRange dateRange, DateTime firstDay, DateTime lastDay)
+    {
+        
+        if (dateRange.EndDate < firstDay)
+        {
+            return 0;
+        }
+
+        if (dateRange.StartDate > lastDay)
+        {
+            return 0;
+        }
+
+        return 32;
+    }
+}
+internal class DateRange
+{
+    public DateTime StartDate { get; }
+    public DateTime EndDate { get; }
+
+    public DateRange(DateTime startDate, DateTime endDate)
+    {
+        StartDate = startDate;
+        EndDate = endDate;
+    }
+}
